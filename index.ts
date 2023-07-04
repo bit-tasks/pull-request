9d0e5784a8f28ca63105507f10d35eea46751c08
ashanfernando <ashan256@gmail.com>
2023-07-04 22:20:39 +0530

    Add branch name variable

--- index.ts
+++ index.ts
@@ -6,7 +6,7 @@ import run, { ExecFunction } from "./scripts/pull-request";
 try {
   const wsDir: string = core.getInput("ws-dir") || process.env.WSDIR || "./";
   const stdExec: ExecFunction = (command: string, options?: {cwd: string}): Promise<number> => exec(command, [], options);
-  const laneName = context?.payload?.pull_request?.number;
+  const laneName = context?.payload?.pull_request?.number.toString() || "test";
   run(stdExec, laneName, wsDir);
 } catch (error) {
   core.setFailed((error as Error).message);
