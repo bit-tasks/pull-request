241323b4ae7d5a317c9f8a502ba65c71f80e31ce
Ashan Fernando <ashan256@gmail.com>
2025-01-16 22:05:48 +0530

    Update get-hello-world.ts

--- test-data/test-scope/get-hello-world/get-hello-world.ts
+++ test-data/test-scope/get-hello-world/get-hello-world.ts
@@ -2,7 +2,7 @@
 type Language = 'en' | 'es';
 
 /**
- * returns a 'hello world' message, in different languages
+ * returns a 'hello world' message, in different languages 1
  */
 export function getHelloWorld(language: Language = 'en') {
   if (language === 'es') return '¡Hola mundo!';
