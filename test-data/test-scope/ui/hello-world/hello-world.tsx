6d34cb235b9b23a0bd38455e1bff2ee2fe640096
Ignacio Aldama Vicente <sr.drabx@gmail.com>
2025-03-27 16:03:15 +0100

    update test data

--- test-data/test-scope/ui/hello-world/hello-world.tsx
+++ test-data/test-scope/ui/hello-world/hello-world.tsx
@@ -1,9 +1,9 @@
-import { getHelloWorld } from '@bit-tasks/test-scope.get-hello-world';
+import { getHelloWorld } from "@bit-tasks/test-scope.get-hello-world";
 
 /**
- * renders a "hello world" text 3
+ * renders a "hello world" text 4
  */
 
 export function HelloWorld() {
-  return <div>{getHelloWorld()}</div>;
+	return <div>{getHelloWorld()}</div>;
 }
